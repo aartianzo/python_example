3c234b1a581cc1729449d01e0a047087cc491f1a
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-08 12:42:10 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,10 +1,6 @@
-def sum(a,b):
-    sum=a+b
-    if sum >10:
-        return "sum less than 10"
-    else:
-        return sum
+def add(a, b):
+    return a + b
 
 
-summed = sum(5,7)
-print(summed)
+def test_add():
+    assert add(2, 3) == 5
