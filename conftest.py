1b6c8947f15a4f80ce83ad52f91edca5fc5438a2
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 12:58:13 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,6 +9,6 @@ def pytest_addoption(parser):
 def add(request):
     add_value1 = request.config.getoption.num1
     add_value2 = request.config.getoption.num2
-    if add_value1 or add_value2 is greater than 10:
+    if add_value1 or add_value2 is less than 10:
         pytest.skip()
-    return params
+    return add
