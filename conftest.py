5a038ed23f6608253e6d8218b299fd97f0611370
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 11:07:50 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,14 +2,14 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--username", action="store", help="input useranme")
-    parser.addoption("--password", action="store", help="input password")
+    parser.addoption("--num1", action="store", help="input num1")
+    parser.addoption("--num2", action="store", help="input num2")
 
 @pytest.fixture
 def params(request):
     params = {}
-    params['username'] = request.config.getoption('--username')
-    params['password'] = request.config.getoption('--password')
-    if params['username'] is None or params['password'] is None:
-        pytest.skip()
+    params['num1'] = request.config.getoption('--num1')
+    params['num2'] = request.config.getoption('--num2')
+#     if params['username'] is None or params['password'] is None:
+#         pytest.skip()
     return params
