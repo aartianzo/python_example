66668cc25c1c78f536489419036c1722753eeadb
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 11:09:41 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,6 +10,6 @@ def params(request):
     params = {}
     params['num1'] = request.config.getoption('--num1')
     params['num2'] = request.config.getoption('--num2')
-#     if params['username'] is None or params['password'] is None:
-#         pytest.skip()
+    if params['num1'] is None or params['num2'] is None:
+        pytest.skip()
     return params
