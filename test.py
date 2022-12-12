2a483a82c31147178e51f6609f67a2b61ffc6d61
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 16:15:06 +0530

    Update and rename argument1.py to test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+def test_arguments(num1):
+    response = requests.get({num1})
+    print(response.request.num1)
+def test_arguments(num2):
+    response = requests.get({num2})
+    print(response.request.num2)
+    assert.
+    
