ea84c18777fd25cf084e422dc7a0d454a61e53dc
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 17:09:05 +0530

    Update updatedconftest.py

--- updatedconftest.py
+++ updatedconftest.py
@@ -7,10 +7,3 @@ def pytest_addoption(parser):
         '--num2',action='store'
     )
 
-def test_arguments(num1):
-    response = request.get(num1)
-    print(response.request.num1)
-def test_arguments(num2):
-    response = request.get(num2)
-    print(response.request.num2)
-    
