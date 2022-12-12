70da7fc1ffb4673d542358c85979498c04d3b5b8
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 12:22:11 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,6 +10,6 @@ def params(request):
     params = {}
     params['num1'] = request.config.getoption('--num1')
     params['num2'] = request.config.getoption('--num2')
-    if params['num1'] is None or params['num2'] is None:
+    if params['num1'],params['num2'] is greater than 10:
         pytest.skip()
     return params
