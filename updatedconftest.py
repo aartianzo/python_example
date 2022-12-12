1bf2cbf5851fd70bcc0c611f5d0d8e8305f4888a
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 16:59:07 +0530

    Update updatedconftest.py

--- updatedconftest.py
+++ updatedconftest.py
@@ -7,9 +7,9 @@ def pytest_addoption(parser):
     )
 
 def test_arguments(num1):
-    response = request.get({num1})
+    response = request.get(num1)
     print(response.request.num1)
 def test_arguments(num2):
-    response = request.get({num2})
+    response = request.get(num2)
     print(response.request.num2)
     
