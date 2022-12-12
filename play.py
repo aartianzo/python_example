084f90c7c1a773fd817bf220811a3568bc480c56
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 14:43:03 +0530

    Update play.py

--- play.py
+++ play.py
@@ -1,6 +1,2 @@
-# test/play.py
-
-def test_params(params):
-    print(params)
-    assert params['username'] == 'test@gmail.com'
-    assert params['password'] == '12345'
+def test_name(name):
+    assert name == 'almond'
