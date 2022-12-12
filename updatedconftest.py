be609798fbaaff6d9b4eac7c975f7c68a871304b
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 16:49:36 +0530

    Update updatedconftest.py

--- updatedconftest.py
+++ updatedconftest.py
@@ -5,8 +5,11 @@ def pytest_addoption(parser):
     parser.addoption(
         '--num2',action='store'
     )
-pytest.fixture
-def num1(request):
-    return request.config.getoption('--num1')
-def num2(request):
-    return request.config.getoption('--num2')
+
+def test_arguments(num1):
+    response = request.get({num1})
+    print(response.request.num1)
+def test_arguments(num2):
+    response = request.get({num2})
+    print(response.request.num2)
+    
