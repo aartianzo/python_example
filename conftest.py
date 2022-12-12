ac45eed20d155616ee6b1b2eac17b026901dbe28
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 12:57:45 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,14 +2,13 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--num1", action="store", help="input num1")
-    parser.addoption("--num2", action="store", help="input num2")
+    parser.addoption("--num1", action="store")
+    parser.addoption("--num2", action="store")
 
 @pytest.fixture
-def params(request):
-    params = {}
-    params['num1'] = request.config.getoption('--num1')
-    params['num2'] = request.config.getoption('--num2')
-    if params['num1'] or params['num2'] is greater than 10:
+def add(request):
+    add_value1 = request.config.getoption.num1
+    add_value2 = request.config.getoption.num2
+    if add_value1 or add_value2 is greater than 10:
         pytest.skip()
     return params
