e1f5bb8fec6e7b045c59ffe95a2b0ddcaec3f9c6
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 16:47:10 +0530

    updatedtest.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 def test_arguments(num1):
-    response = requests.get({num1})
+    response = request.get({num1})
     print(response.request.num1)
 def test_arguments(num2):
-    response = requests.get({num2})
+    response = request.get({num2})
     print(response.request.num2)
     
     
