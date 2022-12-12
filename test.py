7017d1cfdb15d5eb20c1deb0f93eaa5939c911d5
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-12 16:57:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ def num2(request):
     return request.config.getoption('--num2')
 
 def test_arguments(num1):
-    response = request.get({num1})
+    response = request.get(num1)
     print(response.request.num1)
 def test_arguments(num2):
-    response = request.get({num2})
+    response = request.get(num2)
     print(response.request.num2)
     
     
