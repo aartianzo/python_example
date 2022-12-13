01edc140552f215967d9007ae6d1eca1ef5e2d01
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-13 15:18:53 +0530

    Update Pyt1.py
    Adding parameters to the test_add function

--- Pyt1.py
+++ Pyt1.py
@@ -1,8 +1,6 @@
 def sum(a, b):
     return a + b
 
-
-def test_add():
-    assert sum(3,4) < 10
-    assert sum(3,5) > 10, ("sum less than 10")
+def test_add(a, b):
+    assert sum(a, b) > 10, ("sum less than 10")
    
