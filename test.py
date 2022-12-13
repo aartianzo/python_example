1bd2db71d8f2246ec132e201c62a43d6097b4e90
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-13 10:58:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-import pytest
-pytest.fixture
-def num1(request):
-    return request.config.getoption('--num1')
-def num2(request):
-    return request.config.getoption('--num2')
-
-def test_arguments(request):
-    response = request.get(num1)
-    print(response.request.num1)
-def test_arguments(request):
-    response = request.get(num2)
-    print(response.request.num2)
-    
-    
+def test_compute(param1):
+    assert param1 < 4
