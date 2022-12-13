a52a5427d6151a1136cf15610b9b66ebf8980f32
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-13 15:41:07 +0530

    Delete conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-from pytest_easy_addoption import AddOption
-
-class FooBarAddOption(AddOption):
-    foo: str
-    bar: str = 'BAR'
-
-def pytest_addoption(parser):
-    FooBarAddOption.register(parser)
