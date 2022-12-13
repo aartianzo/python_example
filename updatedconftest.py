a156ab91228ef214af40b1d7ee0daad9b07bafd4
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-13 10:59:48 +0530

    Update updatedconftest.py

--- updatedconftest.py
+++ updatedconftest.py
@@ -1,9 +1,11 @@
-import pytest
 def pytest_addoption(parser):
-    parser.addoption(
-        '--num1', action='store'
-    )
-    parser.addoption(
-        '--num2',action='store'
-    )
+    parser.addoption("--all", action="store_true", help="run all combinations")
 
+
+def pytest_generate_tests(metafunc):
+    if "param1" in metafunc.fixturenames:
+        if metafunc.config.getoption("all"):
+            end = 5
+        else:
+            end = 2
+        metafunc.parametrize("param1", range(end))
