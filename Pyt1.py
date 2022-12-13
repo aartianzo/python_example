69465425113b2f81c3279f031d27eebed50309a0
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-13 16:20:52 +0530

    Update Pyt1.py

--- Pyt1.py
+++ Pyt1.py
@@ -1,10 +1,10 @@
-import sys
+# import sys
 
-a = sys.argv[1]
-b = sys.argv[2]
+# a = sys.argv[1]
+# b = sys.argv[2]
 
 def sum(a, b):
     return a + b
 
-def test_add(a, b):
-    assert sum(a, b) > 10, ("sum less than 10")
+def test_add():
+    assert sum(2, 3) > 10, ("sum less than 10")
