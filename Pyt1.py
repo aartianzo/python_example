7c4fec7aa35226683c5ee88e0de87db386cfeb75
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-13 22:00:44 +0530

    Update Pyt1.py

--- Pyt1.py
+++ Pyt1.py
@@ -8,10 +8,10 @@ def pytest_addoption(parser):
 # a = sys.argv[1]
 # b = sys.argv[2]
 @pytest.fixture
-def sum(a, b):
+def sum(--a,--b):
     return a + b
 
 def test_add():
-    assert sum(a, b) > 10, ("sum less than 10")
+    assert sum(--a,--b) > 10, ("sum less than 10")
 #     assert sum(13, 4) == 17
     
