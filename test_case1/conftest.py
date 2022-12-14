68a83e3bb8a8e5d9eefc178fb7990ed91b2e0711
aartianzo <118527920+aartianzo@users.noreply.github.com>
2022-12-14 11:38:24 +0530

    Update conftest.py

--- test_case1/conftest.py
+++ test_case1/conftest.py
@@ -1,1 +1,14 @@
+import pytest
 
+def pytest_addoption(parser):
+  parser.addoption("--num1", action="store", default=5, help="test num1")
+  parser.addoption("--num2", action="store", default=0, help="test num2 ")
+  
+  
+@pytest.fixture
+def num1(request):
+  return int(request.config.getoption("--num1"))
+
+@pytest.fixture
+def num2(request):
+  return int(request.config.getoption("--num2"))
